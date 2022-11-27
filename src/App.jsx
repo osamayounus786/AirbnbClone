ca45df134b629e1cec3beb7c0100ea282c7bd27f
Osama M.Younus <osamayounus541@gmail.com>
2022-11-27 13:33:30 +0500

    destructure the props

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import './App.css';
+import {NavImg} from './components/navbar';
+import {Hero} from './components/hero';
+import {Card} from './components/card';
+import card1 from './assets/images/Card1.png'
+import img from './assets/images/Card2.png'
+import card3 from './assets/images/Card3.png'
+import imgicon from './assets/images/star.png'
+function App() {
+  return (
+    <>
+    <NavImg />
+    <Hero />
+    <div className='card-parent'>
+
+    <Card img= {card1}
+          imgicon = {imgicon}
+          rating= '5.0'
+          number= '(6) ●USA'
+          para= 'Life lesson with Kattie Zeferes'
+          para2= 'From $126'
+            />
+
+    <Card img= {img}
+          imgicon = {imgicon}
+          rating= '5.0'
+          number= '(30) ●USA'
+          para= 'Learn wedding photography'
+          para2= 'From $125'
+            />
+     <Card img= {card1}
+          imgicon = {imgicon}
+          rating= '4.8'
+          number= '(2) ●USA'
+          para= 'Group Mountain Biking'
+          para2= 'From $50'
+          />       
+          </div>
+    </>
+  
+  );
+}
+
+export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,20 @@
+
+function Card({ img,imgicon,rating,number,para,para2}){
+    return(
+        <>
+        <div className="card">
+            <img src={img} alt="wedding" className='card-img' />
+            <div className='card-stats'>
+                <img src={imgicon} alt="rating star" />
+                <span>{rating}</span>
+                <span className='gray'>{number}</span>
+                </div>
+                <p>{para}</p>
+                <p><span className='bold'>{para2}</span> / peroson</p>
+            
+        </div>
+        </>
+    )
+}
+
+export {Card};
